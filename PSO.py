338a37536cdb167093907f7a337cd92762e0face
HansBambel <HansBambel@gmx.net>
2019-02-06 16:25:45 +0100

    Started with swarm

--- PSO.py
+++ PSO.py
@@ -6,10 +6,12 @@ from mpl_toolkits.mplot3d import Axes3D
 def rosenbrock(x, y):
     return (1 - x)**2 + 100*(y - x**2)**2
 
+
 def rastrigin(x, y):
     return 10*2 * np.sum(x**2.0 - 10*np.cos(2*np.pi*x) + y**2.0 - 10*np.cos(2*np.pi*y))
 
-def test_rast(plot_3d=False):
+
+def try_rast(plot_3d=False):
     X_rast = np.arange(-5, 5, 0.1)
     Y_rast = np.arange(-5, 5, 0.1)
     X_rast, Y_rast = np.meshgrid(X_rast, Y_rast)
@@ -28,7 +30,8 @@ def test_rast(plot_3d=False):
         plt.colorbar()
     plt.show()
 
-def test_rosen(plot_3d=False):
+
+def try_rosen(plot_3d=False):
     X_rosen = np.arange(-2, 2, 0.1)
     Y_rosen = np.arange(-1, 3, 0.1)
     X_rosen, Y_rosen = np.meshgrid(X_rosen, Y_rosen)
@@ -49,18 +52,59 @@ def test_rosen(plot_3d=False):
 
 
 def particleSwarm(particles=100, a=0.9, b=2, c=2, benchmark='rosenbrock'):
+    fig = plt.figure()
+    ### Init Particles
     if benchmark == 'rosenbrock':
-        partPos = np.array(np.random.uniform(-2, 2, size=particles),
-                           np.random.uniform(-1, 3, size=particles))
+        partPos = np.array([np.random.uniform(-2, 2, size=particles),
+                           np.random.uniform(-1, 3, size=particles)]).T
+        partVelocity = np.array([np.random.uniform(-4, 4, size=particles),
+                                np.random.uniform(-4, 4, size=particles)]).T
+        benchmarkFunction = lambda x, y: rosenbrock(x, y)
     elif benchmark == 'rastrigin':
-        partPos = np.array(np.random.uniform(-5, 5, size=particles),
-                           np.random.uniform(-5, 5, size=particles))
+        partPos = np.array([np.random.uniform(-5, 5, size=particles),
+                           np.random.uniform(-5, 5, size=particles)]).T
+        partVelocity = np.array([np.random.uniform(-10, 10, size=particles),
+                                np.random.uniform(-10, 10, size=particles)]).T
+        benchmarkFunction = lambda x, y: rastrigin(x, y)
     else:
-        partPos = np.array(np.random.uniform(-2, 2, size=particles),
-                           np.random.uniform(-1, 3, size=particles))
+        partPos = np.array([np.random.uniform(-2, 2, size=particles),
+                           np.random.uniform(-1, 3, size=particles)]).T
+        partVelocity = np.array([np.random.uniform(-4, 4, size=particles),
+                                np.random.uniform(-4, 4, size=particles)]).T
+        benchmarkFunction = lambda x, y: rosenbrock(x, y)
 
     partBestPos = np.copy(partPos)
+    globBestPos = partBestPos[0]
+    globBest = benchmarkFunction(globBestPos[0], globBestPos[1])
+    for p in partBestPos:
+        tmp = benchmarkFunction(p[0], p[1])
+        if tmp < globBest:
+            globBest = tmp
+            globBestPos = p
+
+    ### Let the particles do its thang
+    maxIterations = 100
+    for iter in range(maxIterations):
+        for i, p in enumerate(partPos):
+            for d in range(len(partPos[i])):
+                rP = np.random.randint(0, 2)
+                rG = np.random.randint(0, 2)
+                # Update velocity
+                partVelocity[i, d] = a*partVelocity[i, d] + \
+                                     b*rP*(partBestPos[i, d] - p[d]) + \
+                                     c*rG*(globBestPos[d] - p[d])
+            # new Particle Position
+            partPos += partVelocity
+            if benchmarkFunction(p[0], p[1]) < benchmarkFunction(partBestPos[i, 0], partBestPos[i, 1]):
+                partBestPos = p
+                if benchmarkFunction(partBestPos[0], partBestPos[1]) < globBest:
+                    globBest = benchmarkFunction(partBestPos[0], partBestPos[1])
+                    globBestPos = p
+
+        # Plot the particles
+        plt.scatter(partPos[:, 0], partPos[:, 1])
+        plt.draw()
+        plt.pause(0.001)
 
 
-
-test_rosen(True)
+particleSwarm()
